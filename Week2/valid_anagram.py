1042ebc30dc0104ad0add4dbf24ac53e8524eef5
lijie <lijie7@venusgroup.com.cn>
2020-11-29 23:17:17 +0800

    有效的字母异位词

---
+++ Week2/valid_anagram.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        table = {}
+        for x in s:
+            if x not in table:
+                table[x] = 0
+            table[x] += 1
+
+        for y in t:
+            if y not in table:
+                return False
+            table[y] -= 1
+            if table[y] < 0:
+                return False
+        return True
+
+    def isAnagram2(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
