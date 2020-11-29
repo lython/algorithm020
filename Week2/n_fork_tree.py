d0f7d25d5f31b23ca8944348ddedac4f5a639048
lijie <lijie7@venusgroup.com.cn>
2020-11-30 01:06:06 +0800

    N叉树的前序遍历

--- Week2/n_fork_tree.py
+++ Week2/n_fork_tree.py
@@ -1,8 +1,29 @@
-import unittest
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# N叉树的前序遍历
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
 
-if __name__ == '__main__':
-    unittest.main()
+# Definition for a Node.
+class NodeTree(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: NodeTree
+        :rtype: List[int]
+        """
+        traversal_path = []
+
+        def dfs(node):
+            if node:
+                traversal_path.append(node.val)
+                if node.children:
+                    for chi in node.children:
+                        dfs(chi)
+
+        dfs(root)
+        return traversal_path
